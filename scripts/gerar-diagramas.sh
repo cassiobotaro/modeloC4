ba88fee17c0cfb750545d76d25fa074e9c0dc441
cassiobotaro <cassiobotaro@gmail.com>
2024-03-29 13:14:18 -0300

    Change diagram generation strategy

--- scripts/gerar-diagramas.sh
+++ scripts/gerar-diagramas.sh
@@ -13,13 +13,14 @@ function _gerar_markdown() {
 	echo ""
 }
 
-nome=$1
+lista=(relacionamentos icones cms imersaofc intencoes maonamassa niveis)
+for nome in "${lista[@]}"; do
+	/usr/local/structurizr-cli/structurizr.sh export \
+		-format mermaid \
+		-workspace "$nome"/"$nome".dsl
 
-/usr/local/structurizr-cli/structurizr.sh export \
-	-format mermaid \
-	-workspace "$nome"/"$nome".dsl
-
-[ -f "$nome"/diagramas.md ] && rm "$nome"/diagramas.md
-for arquivo_mmd in "$nome"/*.mmd; do
-	_gerar_markdown "$arquivo_mmd" >>"$nome"/diagramas.md
+	[ -f "$nome"/diagramas.md ] && rm "$nome"/diagramas.md
+	for arquivo_mmd in "$nome"/*.mmd; do
+		_gerar_markdown "$arquivo_mmd" >>"$nome"/diagramas.md
+	done
 done
